f7f693191efd39ae18a9194693018bfb1fe83977
Reg Wang <regrecall@gmail.com>
2012-12-06 13:48:44 +0800

    Initial commit for the imps testing rails server

---
+++ Rails/ImpsMockServer/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ImpsMockServer::Application.config.secret_token = '73123ba9a24c20c9ddd6c560d5272d4734296a37a0c883d6078c54b28d7580b9a9bfbef2d64187f045f4e300f2939949fb1d6e40201ab4e1efc74c86532b447a'
